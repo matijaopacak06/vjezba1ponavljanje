6f4a70d6f1dd9ae1e3dfa4732c9434345fe5cf23
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-01-23 11:57:41 +0100

    Add files via upload

---
+++ vjezba1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanjeVjezba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 2;
+            double b = 3;
+            double c = 4;
+            double d = 5;
+            double x = (a+a+b*b+c*c)/(a*a)+(b*b)+(c*c)+(d*d);
+            Console.WriteLine("rezultat je:" +x);
+            Console.ReadKey();
+
+        }
+    }
+}
